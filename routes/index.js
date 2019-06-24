358c8b6ec32bcd6cbff5da11f71291b14a197ed8
Kazuma Nishigaki <nishigaki7315@outlook.com>
2019-06-24 20:21:18 +0900

    残留申請

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,23 @@ router.get('/', function(req, res, next) {
   }
   res.render('common/temp', _data);
 });
+
+router.get('/mlabschedule', function(req, res, next) {
+  var _data = {
+    panePath:"/pane/mlabschedule",
+    data:{
+      title: "残留申請"
+    }
+  }
+  res.render('common/temp', _data);
+});
+
 router.get('/login', function(req, res, next) {
   res.render('signin');
+});
+
+router.get('/kiosk/test', function(req, res, next) {
+  res.render('kiosk/test');
 });
 
 module.exports = router;
